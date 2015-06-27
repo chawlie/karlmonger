5b1e379a25b364c4abc8f956ae2e2c06254bc126
chawlie <charliechauvin@gmail.com>
2015-06-27 17:32:42 -0500

    Fixed theme merging subfolder

---
+++ wp-content/themes/karl-monger/sidebar-testimonial.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * The sidebar containing the main widget area
+ *
+ * @package WordPress
+ * @subpackage FoundationPress
+ * @since FoundationPress 1.0
+ */
+
+?>
+<aside id="sidebar" class="small-12 large-4 columns">
+	<h4>“Karl took my novel The Wind Walker right where I wanted it to go. I only wish I had met him before I made some of my movies!”</h4>
+    <p>— TOM CHANEY, author of The Wind Walker, director of Wrath of the Wendigo, and screenwriter of Mosquito</p>
+</aside>

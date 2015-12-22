65f2e3967b4f4f7d552a0c525a9c654d1fcaad74
chawlie <charliechauvin@gmail.com>
2015-12-21 22:33:14 -0600

    Theme is done and ready to show client

--- wp-content/themes/karl-monger/page.php
+++ wp-content/themes/karl-monger/page.php
@@ -12,33 +12,48 @@
  */
 
 get_header(); ?>
+<div class="row">
+	<div class="small-12 columns">
+		<section class="page-hero-image">
+			<?php if ( has_post_thumbnail() ) : ?>
+				<div class="row">
+					<div class="column">
+						<?php the_post_thumbnail(); ?>
+					</div>
+				</div>
+			<?php endif; ?>
+		</section>
+	</div>
+</div>
 
 <div class="row">
-	<div class="small-12 large-8 columns" role="main">
+	<div class="page-wrapper">
+		<div class="small-12 large-8 columns page-content" role="main">
 
-	<?php do_action( 'foundationpress_before_content' ); ?>
+		<?php do_action( 'foundationpress_before_content' ); ?>
 
-	<?php while ( have_posts() ) : the_post(); ?>
-		<article <?php post_class() ?> id="post-<?php the_ID(); ?>">
-			<header>
-				<h1 class="entry-title"><?php the_title(); ?></h1>
-			</header>
-			<?php do_action( 'foundationpress_page_before_entry_content' ); ?>
-			<div class="entry-content">
-				<?php the_content(); ?>
-			</div>
-			<footer>
-				<?php wp_link_pages( array('before' => '<nav id="page-nav"><p>' . __( 'Pages:', 'foundationpress' ), 'after' => '</p></nav>' ) ); ?>
-				<p><?php the_tags(); ?></p>
-			</footer>
-			<?php do_action( 'foundationpress_page_before_comments' ); ?>
-			<?php comments_template(); ?>
-			<?php do_action( 'foundationpress_page_after_comments' ); ?>
-		</article>
-	<?php endwhile;?>
+		<?php while ( have_posts() ) : the_post(); ?>
+			<article <?php post_class() ?> id="post-<?php the_ID(); ?>">
+				<header>
+					<h1 class="entry-title"><?php the_title(); ?></h1>
+				</header>
+				<?php do_action( 'foundationpress_page_before_entry_content' ); ?>
+				<div class="entry-content">
+					<?php the_content(); ?>
+				</div>
+				<footer>
+					<?php wp_link_pages( array('before' => '<nav id="page-nav"><p>' . __( 'Pages:', 'foundationpress' ), 'after' => '</p></nav>' ) ); ?>
+					<p><?php the_tags(); ?></p>
+				</footer>
+				<?php do_action( 'foundationpress_page_before_comments' ); ?>
+				<?php comments_template(); ?>
+				<?php do_action( 'foundationpress_page_after_comments' ); ?>
+			</article>
+		<?php endwhile;?>
 
-	<?php do_action( 'foundationpress_after_content' ); ?>
+		<?php do_action( 'foundationpress_after_content' ); ?>
 
+		</div>
 	</div>
 	<?php get_sidebar( 'testimonial' ); ?>
 </div>
--- wp-content/themes/karl-monger/sidebar-testimonial.php
+++ wp-content/themes/karl-monger/sidebar-testimonial.php
@@ -9,6 +9,7 @@
 
 ?>
 <aside id="sidebar" class="small-12 large-4 columns">
-	<h4>“Karl took my novel The Wind Walker right where I wanted it to go. I only wish I had met him before I made some of my movies!”</h4>
-    <p>— TOM CHANEY, author of The Wind Walker, director of Wrath of the Wendigo, and screenwriter of Mosquito</p>
+	<div id="quote">
+        <?php echo do_shortcode('[testimonialswidget_widget]'); ?>
+    </div>
 </aside>
